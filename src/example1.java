ae8b8aa731f8394ae3cd8ee367ee5ef7e99a1b35
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-08 17:50:14 +0530

    code

---
+++ src/example1.java
@@ -0,0 +1,15 @@
+public class example1 {
+    public static void main(String[] args)
+    {
+        int a =100;
+        System.out.println(a);
+        System.out.println(a+10);
+        System.out.println(a + a);
+        System.out.println(a + 'a');
+        System.out.println(a + "a");
+        System.out.println('a' + 'a');
+        System.out.println('a' + "a");
+        System.out.println("a" + "a");
+        System.out.println(a);
+    }
+}
