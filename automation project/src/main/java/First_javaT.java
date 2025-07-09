a18a2fdef8bd385750a191d4b0c6e41c5c420def
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-09 11:55:08 +0530

    first test

---
+++ automation project/src/main/java/First_javaT.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First_javaT {
+    public static void main (String[] arg) {
+
+        // To open the chrome using simple driver
+
+        //System.setProperty("webdriver.chrome.driver" , " location of the driver ")
+        ChromeDriver driver = new ChromeDriver();
+    }
+
+}
