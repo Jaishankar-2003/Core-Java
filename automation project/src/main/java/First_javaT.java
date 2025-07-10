76dac4a741a454f6d5c022049e443f139827c0fa
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-10 12:43:39 +0530

    more

--- automation project/src/main/java/First_javaT.java
+++ automation project/src/main/java/First_javaT.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -30,6 +31,17 @@ public class First_javaT {
 
         // HANDLING IN SELENIUM
 
+        //enter the full name
+        driver.findElement(By.id("fullName")).sendKeys("jaishankar");
+
+        //append text and press keyword
+        driver.findElement(By.id("join")).sendKeys("person", Keys.TAB);
+
+        //what inside the text box
+        String Value = driver.findElement(By.id("getMe")).getAttribute("value");
+        System.out.println(Value);
+
+        //clear the text
 
 
 
