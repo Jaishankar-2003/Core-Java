b4fa1096df379fb3c93c978d0d40564cf5f45509
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-11 00:09:17 +0530

    first

---
+++ src/relational_operat.java
@@ -0,0 +1,11 @@
+public class relational_operat {
+    public static void main (String[] args){
+
+       int x=100 , y=200 , z=300 ;
+       System.out.println(x>y);
+       System.out.println(y<z);
+       System.out.println(y<=x);
+
+
+    }
+}
