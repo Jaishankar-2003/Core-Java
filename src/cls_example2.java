14c7debc860994b4e579c863964841243c7ff496
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-16 17:56:38 +0530

    ✅ Your latest update message

---
+++ src/cls_example2.java
@@ -0,0 +1,15 @@
+public class cls_example2
+{
+    public static void main (String[] args)
+    {
+        int num = 20;
+        if(num >=0)
+        {
+            System.out.println("The given number is positive");
+        }
+        else
+        {
+            System.out.println("The given number is negative");
+        }
+    }
+}
---
+++ src/cls_example3.java
@@ -0,0 +1,15 @@
+public class cls_example3
+{
+    public static void main (String[] args)
+    {
+        int age = 20;
+        if(age >= 18)
+        {
+            System.out.println("The person eligible for vote");
+        }
+        else
+        {
+            System.out.println("The person is not eligible for vote");
+        }
+    }
+}
