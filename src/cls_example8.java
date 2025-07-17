104481a974230c92488e314f77a5e065d230e0bb
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-17 17:48:00 +0530

    if else if

---
+++ src/cls_example8.java
@@ -0,0 +1,21 @@
+public class cls_example8
+{
+    public static void main (String[] args)
+    {
+        double num = 10;
+        if(num > 200)
+        {
+            num = num + num * 0.60;
+            //System.out.println("The student is eligible for exam");
+        }
+        else if (num < 200)
+        {
+            num = num + num * 0.40;
+        }
+        else
+        {
+            num = num - num * 0.50;
+        }
+        System.out.println("The final value is  " + num);
+    }
+}
