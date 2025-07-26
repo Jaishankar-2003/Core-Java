8cf875052594c47c325ce841d07784cf2b1877dd
jaishankar-2003 <jaishankar98421@gmail.com>
2025-07-26 10:35:18 +0530

    cls 26 jul 25

--- src/practice_2.java
+++ src/practice_2.java
@@ -1,19 +1,31 @@
-public class practice_2 {
+
+// count of odd and even digit
+
+public class practice_2
+{
     public static void main (String[] args)
     {
         int num = 26655;
         int rem = 0;
-        int count = 0;
+        int ecount = 0;
+        int ocount = 0;
         while(num >0)
         {
             rem = num % 10;
             if(rem % 2 == 0)
             {
-                count++;
+                ecount++;
+            }
+            else
+            {
+                ocount++;
             }
             num = num / 10;
         }
-        System.out.println(count);
+        System.out.println(ecount);
+        System.out.println(ocount);
 
     }
+
+
 }
---
+++ src/practice_5.java
@@ -0,0 +1,11 @@
+
+// print of count of digit
+
+public class practice_5
+{
+    public static void main (String[] args)
+    {
+        int num = 234;
+
+    }
+}
