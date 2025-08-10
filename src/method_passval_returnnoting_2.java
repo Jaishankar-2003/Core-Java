e383c16bea46766df2ae1f0ac40e163ae9664b4f
jaishankar-2003 <jaishankar98421@gmail.com>
2025-08-10 20:48:04 +0530

    aug 10

---
+++ src/method_passval_returnnoting_2.java
@@ -0,0 +1,19 @@
+public class method_passval_returnnoting_2
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        m2(a,b);
+    }
+    public static void m2(int a , int b)
+    {
+        System.out.println(a);
+        System.out.println(b);
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
---
+++ src/method_passval_returnnoting_3.java
@@ -0,0 +1,17 @@
+public class method_passval_returnnoting_3
+{
+    public static void main(String[] args)
+    {
+        float a = 21.1f;
+        float b = 23.2f;
+        System.out.println(" main method start");
+        m3(a,b);
+        System.out.println(" main method end");
+    }
+    public static void m3(float a , float b)
+    {
+        System.out.println(" The m3 method ");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
