aedba605dc5f8a0ec121e06f7f1f7b2fb43014f7
jaishankar-2003 <jaishankar98421@gmail.com>
2025-08-10 19:29:22 +0530

    aug 10

---
+++ src/method_first.java
@@ -0,0 +1,14 @@
+public class method_first
+{
+    public static void main (String[] args)
+    {
+        System.out.println("main start");
+        print(); // METHOD DEFINITION
+        print();
+    }
+
+    public static void print() // METHOD DECLARATION
+    {
+        System.out.println("hi print method");
+    }
+}
---
+++ src/method_second.java
@@ -0,0 +1,29 @@
+public class method_second
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hi MAIN");
+        m1();
+        m2();
+        m3();
+        m4();
+        System.out.println("Bye MAIN");
+    }
+    public static void m1()
+    {
+        System.out.println("hi m1");
+    }
+    public static void m2()
+    {
+        System.out.println("hi m2");
+    }
+    public static void m3()
+    {
+        System.out.println("hi m3");
+    }
+    public static void m4()
+    {
+        System.out.println("hi m4");
+    }
+
+}
